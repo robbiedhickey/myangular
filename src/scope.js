c3005733e080b999c9a533ae33ed3bff69ea429f
Robbie Hickey <robbie.d.hickey@gmail.com>
2016-09-08 16:50:45 -0500

    Basic scope watch/digest cycle implemented.

---
+++ src/scope.js
@@ -0,0 +1,36 @@
+'use strict';
+var _ = require('lodash');
+
+function Scope(){
+  this.$$watchers = [];
+}
+
+function initWatchVal() { }
+
+Scope.prototype.$watch = function(watchFn, listenerFn){
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn || function() {},
+    last: initWatchVal
+  };
+  this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$digest = function() {
+  var self = this;
+  var newValue, oldValue;
+  _.forEach(this.$$watchers, function(watcher){
+    newValue = watcher.watchFn(self);
+    oldValue = watcher.last;
+    if(newValue !== oldValue){
+      watcher.last = newValue;
+      watcher.listenerFn(
+        newValue, 
+        // don't leak initWatchValue abstraction 
+        oldValue === initWatchVal ? newValue : oldValue, 
+        self);
+    }
+  });
+};
+
+module.exports = Scope;
