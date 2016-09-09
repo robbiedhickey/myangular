2a95710eea2fd758fd43616a00a69d3db3c9a41d
Robbie Hickey <robbie.d.hickey@gmail.com>
2016-09-08 19:38:46 -0500

    Add support for dependent watchers, requiring the digest to be run multiple times

--- src/scope.js
+++ src/scope.js
@@ -16,9 +16,16 @@ Scope.prototype.$watch = function(watchFn, listenerFn){
   this.$$watchers.push(watcher);
 };
 
-Scope.prototype.$digest = function() {
+Scope.prototype.$digest = function(){
+  var dirty;
+  do {
+    dirty = this.$$digestOnce();
+  } while(dirty);
+}
+
+Scope.prototype.$$digestOnce = function() {
   var self = this;
-  var newValue, oldValue;
+  var newValue, oldValue, dirty;
   _.forEach(this.$$watchers, function(watcher){
     newValue = watcher.watchFn(self);
     oldValue = watcher.last;
@@ -29,8 +36,10 @@ Scope.prototype.$digest = function() {
         // don't leak initWatchValue abstraction 
         oldValue === initWatchVal ? newValue : oldValue, 
         self);
+        dirty = true;
     }
   });
+  return dirty;
 };
 
 module.exports = Scope;
