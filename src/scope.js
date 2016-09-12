9242273c1124a856f74b0d03656071216fea831c
Robbie Hickey <robbie.hickey@msionline.com>
2016-09-12 14:41:28 -0500

    Add basic emit/broadcast behavior.

--- src/scope.js
+++ src/scope.js
@@ -394,4 +394,21 @@ Scope.prototype.$on = function(eventName, listener){
 
   listeners.push(listener);
 };
+
+Scope.prototype.$emit = function(eventName) {
+  this.$$fireEventOnScope(eventName);
+};
+
+Scope.prototype.$broadcast = function(eventName) {
+  this.$$fireEventOnScope(eventName);
+};
+
+Scope.prototype.$$fireEventOnScope = function(eventName) {
+  var listeners = this.$$listeners[eventName] || [];
+
+  _.forEach(listeners, function(listener){
+    listener(); 
+  });
+};
+
 module.exports = Scope;
