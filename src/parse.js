4fc706012bcc1cb941d57462be0ba0479d1c3b9b
Robbie Hickey <robbie.hickey@msionline.com>
2016-09-16 13:39:15 -0500

    Parser impl for floating point numbers

--- src/parse.js
+++ src/parse.js
@@ -18,7 +18,8 @@ Lexer.prototype.lex = function (text) {
 
     while (this.index < this.text.length) {
         this.ch = this.text.charAt(this.index);
-        if (this.isNumber(this.ch)) {
+        if (this.isNumber(this.ch) || 
+            (this.ch === '.' && this.isNumber(this.peek()))) {
             this.readNumber();
         } else {
             throw 'Unexpected next character: ' + this.ch;
@@ -37,7 +38,7 @@ Lexer.prototype.readNumber = function () {
 
     while (this.index < this.text.length) {
         var ch = this.text.charAt(this.index);
-        if (this.isNumber(ch)) {
+        if (ch === '.' || this.isNumber(ch)) {
             number += ch;
         } else {
             break;
@@ -49,6 +50,12 @@ Lexer.prototype.readNumber = function () {
         text: number,
         value: Number(number)
     });
+};
+
+Lexer.prototype.peek = function(){
+    return this.index < this.text.length - 1 ? 
+        this.text.charAt(this.index + 1) : 
+        false;   
 };
 
 function AST(lexer) {
