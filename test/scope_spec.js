79bd58e4ca1a0e2265e0e53fe2b744b6c5fe9ccf
Robbie Hickey <robbie.hickey@msionline.com>
2016-09-16 10:17:12 -0500

    Stop event propagation

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1778,7 +1778,7 @@ describe('Scope', function () {
       scope.$emit('someEvent');
       expect(event.currentScope).toBe(null);
     });
-    
+
     it('sets currentScope to null after propagation on $broadcast', function () {
       var event;
       var scopeListener = function (evt) {
@@ -1787,6 +1787,28 @@ describe('Scope', function () {
       scope.$on('someEvent', scopeListener);
       scope.$broadcast('someEvent');
       expect(event.currentScope).toBe(null);
+    });
+
+    it('does not propagate to parents when stopped', function () {
+      var scopeListener = function (event) {
+        event.stopPropagation();
+      };
+      var parentListener = jasmine.createSpy();
+      scope.$on('someEvent', scopeListener);
+      parent.$on('someEvent', parentListener);
+      scope.$emit('someEvent');
+      expect(parentListener).not.toHaveBeenCalled();
+    });
+
+    it('is received by listeners on current scope after being stopped', function () {
+      var listener1 = function (event) {
+        event.stopPropagation();
+      };
+      var listener2 = jasmine.createSpy();
+      scope.$on('someEvent', listener1);
+      scope.$on('someEvent', listener2);
+      scope.$emit('someEvent');
+      expect(listener2).toHaveBeenCalled();
     });
   });
 });
