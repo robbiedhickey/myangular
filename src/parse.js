ded970f9493f24f0f0e13980c5a617c909d27950
Robbie Hickey <robbie.hickey@msionline.com>
2016-09-16 13:57:37 -0500

    Parser impl for scientific notation

--- src/parse.js
+++ src/parse.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _ = require('lodash');
+
 function parse(expr) {
     var lexer = new Lexer();
     var parser = new Parser(lexer);
@@ -35,13 +37,26 @@ Lexer.prototype.isNumber = function (ch) {
 
 Lexer.prototype.readNumber = function () {
     var number = '';
+    var validChars = ['.', 'e', '-', '+', 'E'];
 
     while (this.index < this.text.length) {
         var ch = this.text.charAt(this.index);
         if (ch === '.' || this.isNumber(ch)) {
             number += ch;
         } else {
-            break;
+            var nextCh = this.peek();
+            var prevCh = number.charAt(number.length - 1);
+            if((ch === 'e' || ch === 'E') && this.isExpOperator(nextCh)) {
+                number += ch;
+            } else if (this.isExpOperator(ch) && prevCh === 'e' &&
+                nextCh && this.isNumber(nextCh)){
+                    number += ch;
+            } else if (this.isExpOperator(ch) && prevCh === 'e' &&
+                (!nextCh || !this.isNumber(nextCh))){
+                    throw 'Invalid exponent';
+            } else {
+                break;
+            }
         }
         this.index++;
     }
@@ -56,6 +71,10 @@ Lexer.prototype.peek = function(){
     return this.index < this.text.length - 1 ? 
         this.text.charAt(this.index + 1) : 
         false;   
+};
+
+Lexer.prototype.isExpOperator = function(ch) {
+    return ch === '-' || ch === '+' || this.isNumber(ch);
 };
 
 function AST(lexer) {
