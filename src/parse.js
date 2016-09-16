39e8ca3c0ca546f78fee3370280b4ecaca48b059
Robbie Hickey <robbie.hickey@msionline.com>
2016-09-16 13:32:47 -0500

    Parse implementation for integers

--- src/parse.js
+++ src/parse.js
@@ -6,38 +6,106 @@ function parse(expr) {
     return parser.parse(expr);
 }
 
-function Lexer(){
+function Lexer() {
 }
 
-Lexer.prototype.lex = function(text){
+Lexer.prototype.lex = function (text) {
     //Tokenization done here
+    this.text = text;
+    this.index = 0;
+    this.ch = undefined;
+    this.tokens = [];
+
+    while (this.index < this.text.length) {
+        this.ch = this.text.charAt(this.index);
+        if (this.isNumber(this.ch)) {
+            this.readNumber();
+        } else {
+            throw 'Unexpected next character: ' + this.ch;
+        }
+    }
+
+    return this.tokens;
 };
 
-function AST(lexer){
+Lexer.prototype.isNumber = function (ch) {
+    return '0' <= ch && ch <= '9';
+};
+
+Lexer.prototype.readNumber = function () {
+    var number = '';
+
+    while (this.index < this.text.length) {
+        var ch = this.text.charAt(this.index);
+        if (this.isNumber(ch)) {
+            number += ch;
+        } else {
+            break;
+        }
+        this.index++;
+    }
+
+    this.tokens.push({
+        text: number,
+        value: Number(number)
+    });
+};
+
+function AST(lexer) {
     this.lexer = lexer;
 }
 
-AST.prototype.ast = function(text) {
+AST.Program = 'Program';
+AST.Literal = 'Literal';
+
+AST.prototype.ast = function (text) {
     this.tokens = this.lexer.lex(text);
-    // AST building done here
+    return this.program();
+};
+
+AST.prototype.program = function () {
+    return {
+        type: AST.Program,
+        body: this.constant()
+    };
+};
+
+AST.prototype.constant = function () {
+    return {
+        type: AST.Literal,
+        value: this.tokens[0].value
+    };
 };
 
 function ASTCompiler(astBuilder) {
     this.astBuilder = astBuilder;
 }
 
-ASTCompiler.prototype.compile = function(text) {
+ASTCompiler.prototype.compile = function (text) {
     var ast = this.astBuilder.ast(text);
-    // AST Compilation done here
+    this.state = { body: [] };
+    this.recurse(ast);
+    /* jshint -W054 */
+    return new Function(this.state.body.join(''));
+    /* jshint +W054 */
 };
 
-function Parser(lexer){
+ASTCompiler.prototype.recurse = function (ast) {
+    switch(ast.type){
+        case AST.Program:
+            return this.state.body.push('return ', this.recurse(ast.body), ';');
+        case AST.Literal:
+            return ast.value;
+    }
+};
+
+function Parser(lexer) {
     this.lexer = lexer;
     this.ast = new AST(this.lexer);
     this.astCompiler = new ASTCompiler(this.ast);
 }
 
-Parser.prototype.parse = function(text){
+Parser.prototype.parse = function (text) {
     return this.astCompiler.compile(text);
 };
 
