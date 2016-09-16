62fa03ca61911d0d6b9f23daedaff456e4b4ee1f
Robbie Hickey <robbie.hickey@msionline.com>
2016-09-16 14:24:57 -0500

    Parser impl for strings

--- src/parse.js
+++ src/parse.js
@@ -2,6 +2,11 @@
 
 var _ = require('lodash');
 
+var ESCAPES = {
+    'n': '\n', 'f': '\f', 'r': '\r', 't': '\t',
+    'v': '\v', '\'': '\'', '"': '"'
+};
+
 function parse(expr) {
     var lexer = new Lexer();
     var parser = new Parser(lexer);
@@ -20,9 +25,12 @@ Lexer.prototype.lex = function (text) {
 
     while (this.index < this.text.length) {
         this.ch = this.text.charAt(this.index);
-        if (this.isNumber(this.ch) || 
+        if (this.isNumber(this.ch) ||
             (this.ch === '.' && this.isNumber(this.peek()))) {
             this.readNumber();
+        }
+        else if (this.ch === '\'' || this.ch === '"') {
+            this.readString(this.ch);
         } else {
             throw 'Unexpected next character: ' + this.ch;
         }
@@ -46,14 +54,14 @@ Lexer.prototype.readNumber = function () {
         } else {
             var nextCh = this.peek();
             var prevCh = number.charAt(number.length - 1);
-            if((ch === 'e' || ch === 'E') && this.isExpOperator(nextCh)) {
+            if ((ch === 'e' || ch === 'E') && this.isExpOperator(nextCh)) {
                 number += ch;
             } else if (this.isExpOperator(ch) && prevCh === 'e' &&
-                nextCh && this.isNumber(nextCh)){
-                    number += ch;
+                nextCh && this.isNumber(nextCh)) {
+                number += ch;
             } else if (this.isExpOperator(ch) && prevCh === 'e' &&
-                (!nextCh || !this.isNumber(nextCh))){
-                    throw 'Invalid exponent';
+                (!nextCh || !this.isNumber(nextCh))) {
+                throw 'Invalid exponent';
             } else {
                 break;
             }
@@ -67,14 +75,54 @@ Lexer.prototype.readNumber = function () {
     });
 };
 
-Lexer.prototype.peek = function(){
-    return this.index < this.text.length - 1 ? 
-        this.text.charAt(this.index + 1) : 
-        false;   
+Lexer.prototype.peek = function () {
+    return this.index < this.text.length - 1 ?
+        this.text.charAt(this.index + 1) :
+        false;
 };
 
-Lexer.prototype.isExpOperator = function(ch) {
+Lexer.prototype.isExpOperator = function (ch) {
     return ch === '-' || ch === '+' || this.isNumber(ch);
+};
+
+Lexer.prototype.readString = function (quote) {
+    this.index++;
+    var string = '';
+    var escape = false;
+    while (this.index < this.text.length) {
+        var ch = this.text.charAt(this.index);
+        if (escape) {
+            if (ch === 'u') {
+                var hex = this.text.substring(this.index + 1, this.index + 5);
+                if (!hex.match(/[\da-f]{4}/i)) {
+                    throw 'Invalid unicode escape';
+                }
+                this.index += 4;
+                string += String.fromCharCode(parseInt(hex, 16));
+            } else {
+                var replacement = ESCAPES[ch];
+                if (replacement) {
+                    string += replacement;
+                } else {
+                    string += ch;
+                }
+            }
+            escape = false;
+        } else if (ch === quote) {
+            this.index++;
+            this.tokens.push({
+                text: string,
+                value: string
+            });
+            return;
+        } else if (ch === '\\') {
+            escape = true;
+        } else {
+            string += ch;
+        }
+        this.index++;
+    }
+    throw 'Unmatched quote';
 };
 
 function AST(lexer) {
@@ -117,13 +165,30 @@ ASTCompiler.prototype.compile = function (text) {
 };
 
 ASTCompiler.prototype.recurse = function (ast) {
-    switch(ast.type){
+    switch (ast.type) {
         case AST.Program:
             return this.state.body.push('return ', this.recurse(ast.body), ';');
         case AST.Literal:
-            return ast.value;
+            return this.escape(ast.value);
     }
 };
+
+ASTCompiler.prototype.escape = function (value) {
+    if (_.isString(value)) {
+        return '\'' +
+            value.replace(this.stringEscapeRegex, this.stringEscapeFn) +
+            '\'';
+    } else {
+        return value;
+    }
+};
+
+ASTCompiler.prototype.stringEscapeFn = function (c) {
+    return '\\u' + ('0000' + c.charCodeAt(0).toString(16)).slice(-4);
+};
+
+ASTCompiler.prototype.stringEscapeRegex = /[^ a-zA-Z0-9]/g;
+
 
 function Parser(lexer) {
     this.lexer = lexer;
